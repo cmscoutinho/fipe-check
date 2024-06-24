b6b047e9ef6198f1745763ff88a3c3b845391e79
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-23 22:05:04 -0300

    add spring-generated project

---
+++ src/main/java/br/com/coutsoft/fipe_check/FipeCheckApplication.java
@@ -0,0 +1,13 @@
+package br.com.coutsoft.fipe_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coutsoft/fipe_check/FipeCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coutsoft.fipe_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
