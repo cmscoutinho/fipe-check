ce5d1d926763342b7d23c5072795de2f05852714
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-23 22:17:56 -0300

    implement CommandLineRunner

--- src/main/java/br/com/coutsoft/fipe_check/FipeCheckApplication.java
+++ src/main/java/br/com/coutsoft/fipe_check/FipeCheckApplication.java
@@ -1,12 +1,20 @@
 package br.com.coutsoft.fipe_check;
 
+import br.com.coutsoft.fipe_check.main.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FipeCheckApplication {
+public class FipeCheckApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(FipeCheckApplication.class, args);
     }
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+	}
 }
--- src/main/java/br/com/coutsoft/fipe_check/main/Main.java
+++ src/main/java/br/com/coutsoft/fipe_check/main/Main.java
@@ -2,6 +2,6 @@ package br.com.coutsoft.fipe_check.main;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
