ac9d53f0dae206d11f04352868d31dccec386c44
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-24 23:34:24 -0300

    change string build to StringBuilder

--- src/main/java/br/com/coutsoft/fipecheck/service/URLBuilder.java
+++ src/main/java/br/com/coutsoft/fipecheck/service/URLBuilder.java
@@ -4,13 +4,13 @@ public class URLBuilder {
     private final String BASE_URL_API = "https://parallelum.com.br/fipe/api/v1/";
 
     public String getFullURL(String ... args) {
-        String fullURL = BASE_URL_API;
+        StringBuilder fullURL =  new StringBuilder(BASE_URL_API);
         String[] suffixes = {"/marcas/", "/modelos/", "/anos/", "/"};
 
         for (int i = 0; i < args.length; i++) {
-            fullURL += args[i] + suffixes[i];
+            fullURL.append(args[i] + suffixes[i]);
         }
 
-        return fullURL;
+        return fullURL.toString();
     }
 }
