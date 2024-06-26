1302773738f2de8532fcce6c8f5f9d419d24cca4
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-25 23:12:26 -0300

    create service class DataConverter

---
+++ src/main/java/br/com/coutsoft/fipecheck/service/DataConverter.java
@@ -0,0 +1,18 @@
+package br.com.coutsoft.fipecheck.service;
+
+import br.com.coutsoft.fipecheck.model.CarData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DataConverter implements IDataConverter {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T getData(String json, Class<T> TClass) {
+        try {
+            return mapper.readValue(json, TClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
