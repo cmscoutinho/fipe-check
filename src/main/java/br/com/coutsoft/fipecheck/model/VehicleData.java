c691449f32168fe62728cfad55220b23ffb399db
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-30 13:15:21 -0300

    create data object for vehicles (VehicleData)

---
+++ src/main/java/br/com/coutsoft/fipecheck/model/VehicleData.java
@@ -0,0 +1,9 @@
+package br.com.coutsoft.fipecheck.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record VehicleData(@JsonAlias("codigo") String code,
+                          @JsonAlias("nome") String name) {
+}
