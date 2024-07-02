47fd826753c908954fd6498f0697ec208022a86e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-01 21:42:34 -0300

    configure toString() in CarData

--- src/main/java/br/com/coutsoft/fipecheck/model/CarData.java
+++ src/main/java/br/com/coutsoft/fipecheck/model/CarData.java
@@ -8,4 +8,9 @@ public record CarData(@JsonAlias("Valor") String price,
                       @JsonAlias("Marca") String brand,
                       @JsonAlias("Modelo") String model,
                       @JsonAlias("AnoModelo") int year) {
+
+    @Override
+    public String toString() {
+        return String.format("Model: %s\nBrand: %s\nYear: %d\nPrice: %s", model, brand, year, price);
+    }
 }
