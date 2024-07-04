f7fb07f20a2b6419cd25ff692113cda15c6cfaf1
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-04 20:28:02 -0300

    remove unused imports

--- src/main/java/br/com/coutsoft/fipecheck/model/ModelData.java
+++ src/main/java/br/com/coutsoft/fipecheck/model/ModelData.java
@@ -3,7 +3,6 @@ package br.com.coutsoft.fipecheck.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
