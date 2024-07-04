e57992c23a68c46cf0cb822ab56e0927165092d0
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-03 22:43:40 -0300

    convert menu call to single Menu object

--- src/main/java/br/com/coutsoft/fipecheck/main/Main.java
+++ src/main/java/br/com/coutsoft/fipecheck/main/Main.java
@@ -23,22 +23,24 @@ public class Main {
 
     public void main() {
 
-        String vehicle = new Menu().vehicleMenu();
+        Menu menu = new Menu();
+
+        String vehicle = menu.vehicleMenu();
         String brandJson = connect(vehicle);
         List<BrandData> brandData = Arrays.asList(new DataConverter().getData(brandJson, BrandData[].class));
         brandData.forEach(System.out::println);
 
-        String brand = new Menu().brandMenu();
+        String brand = menu.brandMenu();
         String modelJson = connect(vehicle, brand);
         ModelData modelData = new DataConverter().getData(modelJson, ModelData.class);
         System.out.println(modelData);
 
-        String model = new Menu().modelMenu();
+        String model = menu.modelMenu();
         String yearJson = connect(vehicle, brand, model);
         List<YearData> yearData = Arrays.asList(new DataConverter().getData(yearJson, YearData[].class));
         yearData.forEach(System.out::println);
 
-        String year = new Menu().yearMenu();
+        String year = menu.yearMenu();
         String finalJson = connect(vehicle, brand, model, year);
 
         CarData carData = new DataConverter().getData(finalJson, CarData.class);
