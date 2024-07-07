f5c105036f6e6acace2d435d2f5b94f4a896b469
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-07 13:40:12 -0300

    refactor ModelFilter to use Data class

--- src/main/java/br/com/coutsoft/fipecheck/service/ModelFilter.java
+++ src/main/java/br/com/coutsoft/fipecheck/service/ModelFilter.java
@@ -1,12 +1,13 @@
 package br.com.coutsoft.fipecheck.service;
 
+import br.com.coutsoft.fipecheck.model.Data;
 import br.com.coutsoft.fipecheck.model.ModelData;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ModelFilter {
-    public static List<ModelData.Data> queryFilter(String query, ModelData modelData) {
+    public static List<Data> queryFilter(String query, ModelData modelData) {
         return modelData.models().stream()
                 .filter(model -> model.name().contains(query))
                 .collect(Collectors.toList());
